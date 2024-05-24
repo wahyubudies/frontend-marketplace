83c9ef21b617797ae497a590c6bbe65ef260c46e
lordgent1 <lordgent@digitalcenter.id>
2024-05-24 19:50:14 +0700

    update list admin

--- src/loader/admin/product.js
+++ src/loader/admin/product.js
@@ -1,8 +1,9 @@
 import Product from "../../models/Product";
 
 const adminProductLoader = async () => {
-    const reply = await Product.getListTable();
+    const reply = await Product.getListTableMerchant()
     return reply.data;
 };
+
 
 export default adminProductLoader;
